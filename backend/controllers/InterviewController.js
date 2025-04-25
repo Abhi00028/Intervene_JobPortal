3125110190e4aa5dfa081b9a20ff28080f7e10f0
Abhi00028 <158179677+Abhi00028@users.noreply.github.com>
2025-04-25 14:39:24 -0400

    Update InterviewController.js

--- backend/controllers/InterviewController.js
+++ backend/controllers/InterviewController.js
@@ -6,69 +6,12 @@ import { oauth2Client } from '../utils/googleAuth.js';
 import { google } from 'googleapis';
 import  Earnings  from "../models/earningsSchema.js";
 
-// import axios from 'axios';
 
-
-// InterviewController.js
 
 // Schedule an interview for an applicant
 import { sendEmail } from "../utils/sendEmail.js";  // Ensure this import is correct
 
 // Schedule an interview for an applicant
-// export const scheduleInterview = catchAsyncErrors(async (req, res, next) => {
-//     const { applicantId, jobId, interviewDate } = req.body;
-
-//     // Validate required fields
-//     if (!applicantId || !jobId || !interviewDate) {
-//         return next(new ErrorHandler("Missing required fields.", 400));
-//     }
-
-//     // Check if applicant exists for this job
-//     const application = await Application.findOne({ _id: applicantId, "jobInfo.jobId": jobId });
-//     if (!application) {
-//         return next(new ErrorHandler("Applicant not found for this job.", 404));
-//     }
-
-//     // Get the applicant's email from the application
-//     const applicant = application.jobSeekerInfo;  // Adjust according to your schema
-//     const candidateEmail = applicant.email; // Get the email of the applicant
-
-//     // Generate interview link
-//     const interviewLink = `https://hakkerrankinterview.com/interview?jobId=${jobId}&applicantId=${applicantId}`;
-
-//     // Create a new interview record
-//     const interview = new Interview({
-//         applicantId,    // Applicant's ID
-//         jobId,          // Job's ID
-//         interviewerId: req.user._id, // The ID of the logged-in interviewer
-//         interviewDate,  // Interview date
-//         interviewLink,  // Interview link
-//     });
-//     await interview.save();
-
-//     // Prepare the email message
-//     const subject = "Interview Scheduled";
-//     const message = `
-//         Hello ${applicant.name},
-//         Your interview has been scheduled for ${interviewDate}.
-//         Please join the interview using the following link:
-//         <a href="${interviewLink}">${interviewLink}</a>
-//         Good luck!
-//     `;
-
-//     // Send the interview notification email
-//     await sendEmail({
-//         email: candidateEmail,
-//         subject: subject,
-//         message: message,
-//     });
-
-//     // Respond with a success message
-//     res.status(200).json({
-//         success: true,
-//         message: "Interview scheduled and email notification sent.",
-//     });
-// });
 
 
 export const scheduleInterview = async (req, res, next) => {
